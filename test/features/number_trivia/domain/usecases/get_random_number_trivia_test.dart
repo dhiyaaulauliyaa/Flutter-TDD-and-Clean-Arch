0397be058ef7d0b7454584a388e84f46567e918f
Ahmad Dhiyaaul Auliyaa <do.nvcbl@gmail.com>
2021-09-19 15:40:19 +0700

    add get random trivia usecase

--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -4,35 +4,36 @@ import 'package:mockito/mockito.dart';
 import 'package:dartz/dartz.dart';
 
 import 'package:clean_arch_tdd_ex/features/number_trivia/domain/repositories/number_trivia_repository.dart';
-import 'package:clean_arch_tdd_ex/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:clean_arch_tdd_ex/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:clean_arch_tdd_ex/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_arch_tdd_ex/core/usecases/usecases.dart';
 
 import 'get_concrete_number_trivia_test.mocks.dart';
 
 @GenerateMocks([NumberTriviaRepository])
 void main() {
   final mockNumberTriviaRepository = MockNumberTriviaRepository();
-  final usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  final usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
   final tNumberTrivia = NumberTrivia(number: 1, text: 'test');
-  final tNumber = 1;
 
   test(
-    'should get trivia for the number from the repository',
+    'should get trivia (for the random number) from the repository',
     () async {
       // arrange
-      when(mockNumberTriviaRepository.getConcreteNumberTrivia(1))
+      when(mockNumberTriviaRepository.getRandomNumberTrivia())
           .thenAnswer((_) async => Right(tNumberTrivia));
 
       // act
-      final result = await usecase(Params(number: 1));
+      final result = await usecase(NoParams());
 
       // assert
       expect(result, Right(tNumberTrivia));
-      verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+      verify(mockNumberTriviaRepository.getRandomNumberTrivia());
       verifyNoMoreInteractions(mockNumberTriviaRepository);
     },
   );
 }
+
 
   /*
   Things to ask
